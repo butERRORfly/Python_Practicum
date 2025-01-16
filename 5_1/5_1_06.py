8484fd7d54dfca6d61751a26c9893be304c3111f
butERRORfly <progamer_228@inbox.ru>
2025-01-16 17:36:08 +0300

    Update project

--- 5_1/5_1_06.py
+++ 5_1/5_1_06.py
@@ -13,8 +13,18 @@ class Rectangle:
         return self.set_1[0], self.set_2[1]
 
     def get_size(self):
+        return round(abs(abs(self.set_1[0]) - abs(self.set_2[0])), 2), round(
+            abs(abs(self.set_1[1]) - abs(self.set_2[1])), 2)
+
+    def move(self, dx, dy):
+        self.set_1 = round(self.set_1[0] + dx, 2), round(self.set_1[1] + dy, 2)
+        self.set_2 = round(self.set_2[0] + dx, 2), round(self.set_2[1] + dy, 2)
+
+
+    def resize(self, width, height):
         ...
 
-
 rect = Rectangle((3.2, -4.3), (7.52, 3.14))
-print(rect.get_pos())
+print(rect.get_pos(), rect.get_size())
+rect.move(1.32, -5)
+print(rect.get_pos(), rect.get_size())
