5557bb1773b9df1aa762e1da66ec63306582b95e
butERRORfly <progamer_228@inbox.ru>
2025-03-16 19:54:19 +0300

    Updated 5_3

--- 5_3/5_3_10.py
+++ 5_3/5_3_10.py
@@ -14,17 +14,26 @@ class NeedCharError(Exception):
     pass
 
 
-def password_validation(password, min_length=8, possible_chars=string.ascii_letters + string.digits,
-                        at_least_one=str.isdigit) -> str:
+def password_validation(
+    password,
+    min_length=8,
+    possible_chars=string.ascii_letters + string.digits,
+    at_least_one=str.isdigit,
+) -> str:
+
     if not isinstance(password, str):
         raise TypeError
+
     if len(password) < min_length:
         raise MinLengthError
+
     if any([char not in possible_chars for char in password]):
         raise PossibleCharError
+
     if not any([at_least_one(x) for x in password]):
         raise NeedCharError
 
     hash_object = sha256()
     hash_object.update(password.encode())
+
     return hash_object.hexdigest()
